804861e9d42576b147fb5740b595d4826cc29379
MarioWerner <mario.g.werner@googlemail.com>
2023-01-31 19:19:55 +0100

    Added join and adressTextField

--- Menu and Lobby Template using Mirror/Assets/Resources/Scripts/Menu/MenuHandler.cs
+++ Menu and Lobby Template using Mirror/Assets/Resources/Scripts/Menu/MenuHandler.cs
@@ -5,12 +5,13 @@ using Visibility = UnityEngine.UIElements.Visibility;
 
 public class MenuHandler : MonoBehaviour
 {
-
+    private Button quitButton;
     private Button joinMenuButton;
     private Button hostButton;
     private Button joinGameButton;
     private VisualElement root;
     private GroupBox joinGameGroup;
+    private TextField adressTextField;
     private void OnEnable()
     {
         root = GetComponent<UIDocument>().rootVisualElement;
@@ -18,6 +19,11 @@ public class MenuHandler : MonoBehaviour
 
         joinGameGroup = root.Q<GroupBox>("joinGameGroup");
 
+        adressTextField = root.Q<TextField>("adressTextField");
+        adressTextField.RegisterCallback<KeyDownEvent>(ev =>
+        {
+            NetworkRoomManager.singleton.networkAddress = adressTextField.text;
+        });
 
         hostButton = root.Q<Button>("hostButton");
         hostButton.RegisterCallback<ClickEvent>(ev => startRoom());
@@ -28,6 +34,12 @@ public class MenuHandler : MonoBehaviour
 
         joinGameButton = root.Q<Button>("joinGameButton");
         joinGameButton.RegisterCallback<ClickEvent>(ev => joinGame());
+
+        quitButton = root.Q<Button>("quitButton");
+        quitButton.RegisterCallback<ClickEvent>(ev => { 
+            Application.Quit();
+            Debug.Log("Application.Quit called!");
+        });
     }
 
     //Toggle Visibility for Adress Input and Join Button
--- Menu and Lobby Template using Mirror/Assets/Resources/Scripts/Menu/RoomUiHandler.cs
+++ Menu and Lobby Template using Mirror/Assets/Resources/Scripts/Menu/RoomUiHandler.cs
@@ -1,20 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Mirror;
+using UnityEngine;
 using UnityEngine.UIElements;
-using System;
 
 public class RoomUiHandler : MonoBehaviour
 {
     private VisualElement root;
     private Button startGameButton;
+    private Button quitButton;
     private void OnEnable()
     {
         root = GetComponent<UIDocument>().rootVisualElement;
 
         startGameButton = root.Q<Button>("startGameButton");
         startGameButton.RegisterCallback<ClickEvent>(ev => startGame());
+
+        quitButton = root.Q<Button>("quitButton");
+        quitButton.RegisterCallback<ClickEvent>(ev =>
+        {
+
+            NetworkRoomManager.singleton.StopClient();
+            NetworkRoomManager.singleton.StopHost();
+            NetworkRoomManager.singleton.StopServer();
+
+        });
 
     }
 
